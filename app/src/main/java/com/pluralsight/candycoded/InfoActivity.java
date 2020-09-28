41f304e54daf385b2066fdb6d377e54c07a88e1f
JemimaNaomiBen <54313529+JemimaNaomiBen@users.noreply.github.com>
2020-09-28 17:38:29 +0100

    Created Intent to start PhoneActivity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -43,4 +43,11 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view){
+        Uri phoneURI = Uri.parse("tel:0123456789");
+
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(phoneURI);
+        startActivity(phoneIntent);
+    }
 }
